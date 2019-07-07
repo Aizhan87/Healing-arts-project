4041e13736bc30b54768683fef08a3b45ac704c4
Aizhan87 <Aizhan87@aizhans-mbp.home>
2019-07-06 19:14:33 -0700

    adjusted routing to client side

--- routes.js
+++ routes.js
@@ -1,12 +1,12 @@
 module.exports = function (app) {
 
-	// const customer = require('./routes/customer');
+	const customer = require('./routes/customer');
 	const application = require('./routes/application');
 	const users = require('./routes/users');
 	const trips = require('./routes/trips');
 	const pricing = require('./routes/pricing');
 
-	// app.use('/', customer);
+	app.use('/', customer);
 	app.use('/user', application);
 	app.use('/users', users);
 	app.use('/trips', trips);
