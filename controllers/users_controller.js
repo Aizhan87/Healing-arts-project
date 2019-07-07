d54a3d57a25ea7ddcc3edfe2092a8d998db30d23
Aizhan87 <Aizhan87@aizhans-mbp.home>
2019-07-07 08:20:32 -0700

    redirected /user to /admin

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -9,7 +9,7 @@ exports.registrationPage = function (req, res) {
 
 exports.signOutUser = function (req, res) {
   req.logout();
-  res.redirect("/user");
+  res.redirect("/admin");
 };
 
 // login
@@ -17,7 +17,7 @@ exports.loginUser = function (req, res) {
   // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
   // So we're sending the user back the route to the members page because the redirect will happen on the front end
   // They won't get this or even be able to access this page if they aren't authed
-  res.json("/user");
+  res.json("/admin");
 };
 
 // register a user
@@ -37,7 +37,7 @@ exports.signUpUser = function (req, res) {
         email: req.body.email,
         password: req.body.password
       }).then(function () {
-        res.send({ redirect: '/user' });
+        res.send({ redirect: '/admin' });
       }).catch(function (err) {
         res.json(err);
       });
