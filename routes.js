f8eb40ba4b72604d39b457ada75d3ae11f7c7411
Aizhan87 <Aizhan87@aizhans-mbp.home>
2019-07-06 20:04:14 -0700

    created handlebars for each section

--- routes.js
+++ routes.js
@@ -1,12 +1,22 @@
 module.exports = function (app) {
 
 	const customer = require('./routes/customer');
+	const about = require('./routes/about');
+	const team = require('./routes/team');
+	const services = require('./routes/services');
+	const appointment = require('./routes/appointment');
+	const contact = require('./routes/contact');
 	const application = require('./routes/application');
 	const users = require('./routes/users');
 	const trips = require('./routes/trips');
 	const pricing = require('./routes/pricing');
 
 	app.use('/', customer);
+	app.use('/about', about);
+	app.use('/team', team);
+	app.use('/services', services);
+	app.use('/appointment', appointment);
+	app.use('/contact', contact);
 	app.use('/user', application);
 	app.use('/users', users);
 	app.use('/trips', trips);
